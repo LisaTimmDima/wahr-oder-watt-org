3105d387e7bb8d7156adc335d46ac5cd7a4831ae
iiakovleva-l <ieliakovleva@gmail.com>
2025-09-09 16:00:40 +0200

    VERSION: 1.0.9

--- backend/src/main/java/com/school/project/wahr_oder_watt/config/WebConfig.java
+++ backend/src/main/java/com/school/project/wahr_oder_watt/config/WebConfig.java
@@ -1,22 +1,17 @@
 package com.school.project.wahr_oder_watt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:./static/");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/static/index.html");
-        registry.addViewController("/favicon.ico").setViewName("forward:/static/favicon.ico");
     }
 }
