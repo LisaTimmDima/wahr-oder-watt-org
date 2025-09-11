8888229659b616f0cc70bcfafaf5443eac6e1707
iiakovleva-l <ieliakovleva@gmail.com>
2025-09-11 09:54:39 +0200

    rolle

--- frontend/src/views/AdminUserManagement.vue
+++ frontend/src/views/AdminUserManagement.vue
@@ -283,7 +283,7 @@ onMounted(async () => {
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Admin</th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rolle</th>
                 <th class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Aktion</th>
                 </tr>
             </thead>
